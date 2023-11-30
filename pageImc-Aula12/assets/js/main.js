a0875240efe9decd517da0b8a1efdb729a7df62f
caiofariaas <caiofarias077@gmail.com>
2023-11-30 15:13:17 -0300

    js

--- pageImc-Aula12/assets/js/main.js
+++ pageImc-Aula12/assets/js/main.js
@@ -3,12 +3,25 @@ const form = document.querySelector('#formulario')
 // Adicionando uma função anonima junto ao addEvent...
 
 form.addEventListener('submit', function(e){
+
+// Impede que eventos padrões sejam executados, por exemplo o reload da pagina
+
     e.preventDefault();
+
+// e.targer indica quem está recebendo este evento
+// no caso colocamos o ID do elemento
+// retorna o elemento todo
+
     const inputPeso = e.target.querySelector('#peso');
     const inputAltura = e.target.querySelector('#altura');
 
+// Transforma os valores dos input's a um Number
+
     const peso = Number(inputPeso.value)
     const altura = Number(inputAltura.value)
+
+// se peso não for 'true' ele mostra o resultado como inválido
+// faz o mesmo com altura, caso ambos não aconteçam, ele executa o IMC
 
     if(!peso){
         setResult('peso inválido', false);
@@ -19,14 +32,19 @@ form.addEventListener('submit', function(e){
         return;
     }
 
+// Chama a fução getImc para fazer o calculo
+
     const imc = getImc(peso, altura);
 
-    console.log(imc);
+// Seta o result com o IMC encontrado
 
+    setResult(`Seu IMC: ${imc}`, true);
 });
 
 
-function criaP(className){
+// Função responsável por criar os elementos 'P'
+
+function criaP(){
     
 // result.innerHTML = `<p>${msg}</p>`;
 
@@ -36,27 +54,49 @@ function criaP(className){
 
 // Adiciona uma classe a tag  P criada
 
-    p.classList.add(className)
-
     return p;
 }
+
+// Função responsável por setar o resultado!
+// recebe uma mensagem e um parametro "true or false'"
 
 function setResult(msg, isValid){
     const result = document.querySelector('#result')
     result.innerHTML = '';
 
-    const p = criaP('paragrafoResult');
-    p.innerHTML = msg
+    const p = criaP();
+
+    p.innerHTML = msg;
 
 // Adiciona o elemento ao HTML, como um elemento filho
 
     result.appendChild(p);
 }
 
+// função responsável por fazer o calculo do imc
+
 function getImc(peso, altura){
 
     const IMC = peso / (altura * altura);
 
-    return IMC;
+    return IMC.toFixed(2);
 }
 
+function getNivelImc(imc){
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 
+    'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3']
+
+    if (imc >= 40){
+        return nivel[5];
+    }
+    if (imc >= 35){
+        return nivel[4];
+    }
+    if (imc >= 30){
+        return nivel[3];
+    }
+    if (imc >=25){
+        
+    }
+
+}
