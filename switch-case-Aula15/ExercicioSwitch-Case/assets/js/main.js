baa322d1e3a5663d22b24dcdfcd423e0724f9996
caiofariaas <caiofarias077@gmail.com>
2023-12-08 12:16:54 -0300

    sec

---
+++ switch-case-Aula15/ExercicioSwitch-Case/assets/js/main.js
@@ -0,0 +1,73 @@
+const data = new Date;
+
+console.log(data);
+
+function criarP(){
+  const p = document.createElement('p');
+  return p;
+}
+
+function setText(date){
+  const resultado = document.querySelector('.container');
+
+  resultado.innerHTML = '';
+  const p = criarP();
+
+  p.innerHTML = date;
+
+  resultado.appendChild(p);
+}
+
+
+
+function diaText(){
+
+  const day = data.getDay();
+
+  let txt = ''
+
+  switch(day){
+    case 0: txt = "Domingo"; return txt;
+    case 1: txt = "Segunda-Feira"; return txt;
+    case 2: txt = "Terça-Feira"; return txt;
+    case 3: txt = "Quarta-Feira"; return txt;
+    case 4: txt = "Quinta-Feira"; return txt;
+    case 5: txt = "Sexta-Feira"; return txt;
+    case 6: txt = "Sábado"; return txt;
+  }
+}
+
+function mesText(){
+
+  const mes = data.getMonth();
+
+  let txt = ''
+
+  switch(mes){
+    case 0: txt = "Janeiro"; return txt;
+    case 1: txt = "Fevereiro"; return txt;
+    case 2: txt = "Abril"; return txt;
+    case 3: txt = "Março"; return txt;
+    case 4: txt = "Maio"; return txt;
+    case 5: txt = "Junho"; return txt;
+    case 6: txt = "Julho"; return txt;
+    case 7: txt = "Agosto"; return txt;
+    case 8: txt = "Setembro"; return txt;
+    case 9: txt = "Outubro"; return txt;
+    case 10: txt = "Novembro"; return txt;
+    case 11: txt = "Dezembro"; return txt;
+  }
+}
+
+function dataTotal(day, mes){
+  day = diaText();
+  mes = mesText();
+  const diaNum = data.getDate();
+  const ano = data.getFullYear();
+  const horas = data.getHours();
+  const minutos = data.getMinutes();
+
+  return `${day}, ${diaNum} de ${mes} de ${ano} ${horas}:${minutos}`
+}
+
+setText(dataTotal(diaText(), mesText()));
