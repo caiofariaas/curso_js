a6ff0178f4170741672bba9a9471549e4984c627
caiofariaas <caiofarias077@gmail.com>
2023-12-09 21:44:23 -0300

    sec

--- switch-case-Aula15/ExercicioSwitch-Case/assets/js/main.js
+++ switch-case-Aula15/ExercicioSwitch-Case/assets/js/main.js
@@ -123,7 +123,6 @@ function mesText(){
   const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 
                   'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro',
                    'Outubro', 'Novembro', 'Dezembro'];
-  
   return meses[mes];
 }
 
