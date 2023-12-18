2cb488aff8051ae8534ef09086507a15806e036b
caiofariaas <caiofarias077@gmail.com>
2023-12-18 09:20:58 -0300

    newV

---
+++ fetch-Api-Aula16/api.js
@@ -0,0 +1,61 @@
+const characterId = document.querySelector("#characterId");
+const btnGo= document.querySelector("#btn-go");
+const content = document.querySelector("#content");
+const image = document.querySelector("#img");
+
+// Faz uma requisição na API atraves da função FETCH
+// recebe o ID do usuário
+
+const fetchApi = (value) =>{
+
+// Guarda o resultado da FETCH
+// fetch é a função que recolhe as informações da API
+// Passamos como parametro o ID do personagem
+
+// promise é literalmente uma promessa!
+// o 'then()' é usado para pegar oque vem depois de promisse
+// Conceitos de códigos assincronos
+
+
+// Ele pega o resultado do fetch que vem como promisse
+    const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+
+// depois transformamos o resultado 'JS' em 'json' que tambem é uma promise
+    .then((res) => res.json())
+
+// Quando de fato existir essa saída nos temos o 'data'
+// tambem é possivel trazer algo especifico, no exemplo usei o nome retornado em 'data'
+
+    .then((data) => {
+        console.log(data)
+
+// esse return manda a info a 'result'
+
+        return data;
+    });
+
+// aqui retornamos result como resultado total
+
+    return result;
+}
+
+// Usamos o EventListener para ler um click e com isso chamar a função de 'Fetch'
+
+// async indica que é uma função assincrona!
+// por estarmos recebendo um dado de uma API, isso é necessário
+
+btnGo.addEventListener('click', async (e) =>{
+    e.preventDefault()
+    
+    const result = await fetchApi(characterId.value)
+
+// Aqui como estamos tentando inserir um Objeto 'json', precisamos transforma-lo em 'string' 
+
+// utilizando a tag </pre>
+// 'undefined' não sei
+// 2 para os espaços
+
+    content.innerHTML =  `${JSON.stringify(result, undefined, 2)}`;
+    
+    image.src = `${result.image}`
+});
