bca7d912a917e8a213fbc5558334e05183774da4
caiofariaas <caiofarias077@gmail.com>
2023-12-20 20:45:37 -0300

    JS

--- studying-api/obterListaUser.js
+++ studying-api/obterListaUser.js
@@ -1,7 +1,8 @@
+// Manipulação de resultados
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-// Função recebendo 
+// Função que faz a requisição GET da API
 
 function obterListaUser(){
     return fetch (apiUrl)
@@ -13,11 +14,19 @@ function obterListaUser(){
     })
 }
 
+// função responsável por procurar e mostrar o email e o nome dos usuários
+
 function nomeEmailUser(users){
     users.forEach(user => {
         console.log(`Nome: ${user.name} - Email ${user.email}`);
     });
 }
+
+//  chamamos a função obterListaUser()
+// usamos o then para quando a promise for resolvida nós chamarmos a função responsável por 
+// mostrar apenas o nome e o email do usuário
+// ao invés de passarmos todo o 'data' passamos apenas oque queremos
+// isso é uma manipulação de dados com API
 
 obterListaUser()
 .then(users =>{
