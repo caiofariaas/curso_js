c46734d779e834ea78e6bc3c6aba673bff76a5d1
caiofariaas <caiofarias077@gmail.com>
2023-12-21 08:48:21 -0300

    js

--- studying-api/obterListaUser.js
+++ studying-api/obterListaUser.js
@@ -23,8 +23,9 @@ function nomeEmailUser(users){
 }
 
 //  chamamos a função obterListaUser()
-// usamos o then para quando a promise for resolvida nós chamarmos a função responsável por 
+// usamos o 'then' para quando a promise for resolvida nós chamarmos a função responsável por 
 // mostrar apenas o nome e o email do usuário
+
 // ao invés de passarmos todo o 'data' passamos apenas oque queremos
 // isso é uma manipulação de dados com API
 
